7d95493e509744723706e9a3271bd20963031a8f
gimunaretto <61035475+gimunaretto@users.noreply.github.com>
2024-09-23 21:06:31 -0300

    Hotfix: ajuste de erro "CS0826 No best type found for implicitly-typed array"

--- Program.cs
+++ Program.cs
@@ -12,16 +12,16 @@ namespace TransacaoFinanceira
 
         static void Main(string[] args)
         {
-            var TRANSACOES = new[] { new {correlation_id= 1,datetime="09/09/2023 14:15:00", conta_origem= 938485762, conta_destino= 2147483649, VALOR= 150},
-                                     new {correlation_id= 2,datetime="09/09/2023 14:15:05", conta_origem= 2147483649, conta_destino= 210385733, VALOR= 149},
-                                     new {correlation_id= 3,datetime="09/09/2023 14:15:29", conta_origem= 347586970, conta_destino= 238596054, VALOR= 1100},
-                                     new {correlation_id= 4,datetime="09/09/2023 14:17:00", conta_origem= 675869708, conta_destino= 210385733, VALOR= 5300},
-                                     new {correlation_id= 5,datetime="09/09/2023 14:18:00", conta_origem= 238596054, conta_destino= 674038564, VALOR= 1489},
-                                     new {correlation_id= 6,datetime="09/09/2023 14:18:20", conta_origem= 573659065, conta_destino= 563856300, VALOR= 49},
-                                     new {correlation_id= 7,datetime="09/09/2023 14:19:00", conta_origem= 938485762, conta_destino= 2147483649, VALOR= 44},
-                                     new {correlation_id= 8,datetime="09/09/2023 14:19:01", conta_origem= 573659065, conta_destino= 675869708, VALOR= 150},
-
-            };
+            Transacao[] TRANSACOES = [
+                new Transacao { correlation_id = 1, datetime = "09/09/2023 14:15:00", conta_origem = 938485762, conta_destino = 2147483649, VALOR = 150 },
+                new Transacao { correlation_id = 2, datetime = "09/09/2023 14:15:05", conta_origem = 2147483649, conta_destino = 210385733, VALOR = 149 },
+                new Transacao { correlation_id = 3, datetime = "09/09/2023 14:15:29", conta_origem = 347586970, conta_destino = 238596054, VALOR = 1100 },
+                new Transacao { correlation_id = 4, datetime = "09/09/2023 14:17:00", conta_origem = 675869708, conta_destino = 210385733, VALOR = 5300 },
+                new Transacao { correlation_id = 5, datetime = "09/09/2023 14:18:00", conta_origem = 238596054, conta_destino = 674038564, VALOR = 1489 },
+                new Transacao { correlation_id = 6, datetime = "09/09/2023 14:18:20", conta_origem = 573659065, conta_destino = 563856300, VALOR = 49 },
+                new Transacao { correlation_id = 7, datetime = "09/09/2023 14:19:00", conta_origem = 938485762, conta_destino = 2147483649, VALOR = 44 },
+                new Transacao { correlation_id = 8, datetime = "09/09/2023 14:19:01", conta_origem = 573659065, conta_destino = 675869708, VALOR = 150 },
+            ];
             executarTransacaoFinanceira executor = new executarTransacaoFinanceira();
             Parallel.ForEach(TRANSACOES, item =>
             {
@@ -31,11 +31,11 @@ namespace TransacaoFinanceira
         }
     }
 
-    class executarTransacaoFinanceira: acessoDados
+    class executarTransacaoFinanceira : acessoDados
     {
         public void transferir(int correlation_id, int conta_origem, int conta_destino, decimal valor)
         {
-            contas_saldo conta_saldo_origem = getSaldo<contas_saldo>(conta_origem) ;
+            contas_saldo conta_saldo_origem = getSaldo<contas_saldo>(conta_origem);
             if (conta_saldo_origem.saldo < valor)
             {
                 Console.WriteLine("Transacao numero {0 } foi cancelada por falta de saldo", correlation_id);
@@ -49,6 +49,15 @@ namespace TransacaoFinanceira
                 Console.WriteLine("Transacao numero {0} foi efetivada com sucesso! Novos saldos: Conta Origem:{1} | Conta Destino: {3}", correlation_id, conta_saldo_origem.saldo, conta_saldo_destino.saldo);
             }
         }
+    }
+
+    class Transacao
+    {
+        public int correlation_id;
+        public string datetime;
+        public long conta_origem;
+        public long conta_destino;
+        public decimal VALOR;
     }
     class contas_saldo
     {
@@ -80,13 +89,13 @@ namespace TransacaoFinanceira
 
             SALDOS = new Dictionary<int, decimal>();
             this.SALDOS.Add(938485762, 180);
-           
+
         }
         public T getSaldo<T>(int id)
-        {          
+        {
             return (T)Convert.ChangeType(TABELA_SALDOS.Find(x => x.conta == id), typeof(T));
         }
-        public bool atualizar<T>(T  dado)
+        public bool atualizar<T>(T dado)
         {
             try
             {
@@ -100,7 +109,7 @@ namespace TransacaoFinanceira
                 Console.WriteLine(e.Message);
                 return false;
             }
-            
+
         }
 
     }
